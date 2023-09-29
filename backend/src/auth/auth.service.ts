ab012e6ecd190c3fc706c4914f71341ede24ae1c
AmirAliEidivandi <amirah648887@gmail.com>
2023-09-29 18:46:57 +0330

    init

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async refreshToken() {}
+}
